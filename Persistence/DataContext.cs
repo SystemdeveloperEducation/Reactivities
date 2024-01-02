69723bf89dce4ad0339c8f9019577fea1ec2346f
andres.parra <andres.parra@adlibris.com>
2024-01-02 18:34:38 +0100

    updated to .net8.0 and added Seed
    Updated to .net 8.0 so i dont have to install aspnetcore 7.0.14 runtimes
    Updated the nugets with dotnet outdated -u
    Added Seed.cs so that i can fill the database with data on startup

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -2,6 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
+/// <summary>
+/// The DataContext class is the bridge between the application and the database.
+/// </summary>
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions options) : base(options)
