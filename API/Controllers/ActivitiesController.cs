028a02520a9ff4dc20b431ececa27bccf353b53c
andres.parra <andres.parra@adlibris.com>
2024-01-23 21:41:49 +0100

    added Activities controller and basecontroller
    *The base controller is so that all other controller has base functionality to prever DRY
    *The ActivityController is the first controller to be based on the base controller

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,13 @@
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public ActivitiesController(DataContext context)
+    {
+        _context = context;
+    }
+}
